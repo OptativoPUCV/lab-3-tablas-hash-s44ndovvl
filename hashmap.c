bbafc981d8202a4928ce73cadc9ffe0c04ee5860
Your Name <you@example.com>
2025-04-18 05:39:34 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -185,6 +185,7 @@ Pair * nextMap(HashMap * map) {
     }
 
     // Si no encuentra un par válido, retorna NULL
+    map->current = -1;
     return NULL;
     //siguiente %= map->capacity;
     /*while(map->buckets[siguiente] == NULL || map->buckets[siguiente]->key == NULL){
